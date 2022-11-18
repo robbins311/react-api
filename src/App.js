46602cf1e0317fcaadcad6be42b30136df9d0f45
robbins311 <robbins312@naver.com>
2022-11-18 15:54:48 +0900

    use strictmode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
-import ReactAsyncUsers from "./ReactAsyncUsers";
-// import Users from "./Users";
+// import ReactAsyncUsers from "./ReactAsyncUsers";
+import Users from "./Users";
 
 function App() {
   return (
     <>
-      {/* <Users /> */}
-      <ReactAsyncUsers />
+      <Users />
+      {/* <ReactAsyncUsers /> */}
     </>
   );
 }
